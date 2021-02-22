26c68b81af282d31ce7fe031de47f4cfc0c9c62c
Konu007 <hisnu.almujahidin@unida.gontor.ac.id>
2021-02-22 12:47:08 +0700

    1

---
+++ ELECTRON4PHP-win32-x64/resources/app/README.md
@@ -0,0 +1,23 @@
+# sawit
+
+Aplikasi crud sederhana dengan studi kasus penjualan kelapa sawit dengan entitas petani, sawit, transaksi dan laporan menggunakan perhitungan sederhana untuk memperoleh laporan penjualan kelapa sawit sesuai dengan kebutuhan.
+
+# fitur
+
+1. PHP Mysqli Procedural Connection
+2. Create
+3. Read
+4. Update
+5. Delete
+
+# screenshot
+
+![alt text](https://i.imgur.com/IzIKn9q.png)
+
+![alt text](https://i.imgur.com/bIpvgp9.png)
+
+![alt text](https://i.imgur.com/9ZgvQ9n.png)
+
+![alt text](https://i.imgur.com/jfG7Kis.png)
+
+![alt text](https://i.imgur.com/X8QjjsS.png)
---
+++ ELECTRON4PHP-win32-x64/resources/app/include/header.php
@@ -0,0 +1,22 @@
+	<div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="index.php" class="navbar-brand">Aplikasi Penjualan Buah Dan Sayuran</a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php"><i class='fa fa-home'></i> Beranda</a></li>
+            <li><a href="petani.php"><i class='fa fa-users'></i> Data Petani</a></li>
+            <li><a href="sawit.php"><i class='fa fa-file'></i> Data Buah Dan Sayur</a></li>
+            <li><a href="transaksi.php"><i class='fa fa-plus'></i> Tambah Data Transaksi</a></li>
+            <li><a href="laporan.php"><i class='fa fa-file'></i> Laporan</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
